ce11ac96b2345c05b2a4a8436091c327ee7f5efb
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-02-03 16:22:30 +0000

    Cast integer values for pointers via intptr_t.
    Use intptr_t to cast integer values representing pointers to
    capabilities that can be dereferenced.
    
    Clang generated warnings for these issues:
    warning: cast from provenance-free integer type to pointer type will
    give pointer that can not be dereferenced [-Wcheri-capability-misuse]
    
    Reported by:	jrtc27

--- external/picosat/picosat.c
+++ external/picosat/picosat.c
@@ -145,7 +145,7 @@ IN THE SOFTWARE.
 typedef uintptr_t Wrd;
 #define ISLITREASON(C) (1&(Wrd)C)
 #define LIT2REASON(L) \
-  (assert (L->val==TRUE), ((Cls*)(1 + (2*(L - ps->lits)))))
+  (assert (L->val==TRUE), ((Cls*)(Wrd)(1 + (2*(L - ps->lits)))))
 #define REASON2LIT(C) ((Lit*)(ps->lits + ((Wrd)C)/2))
 #endif
 
