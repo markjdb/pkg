8a2c1445f81ff47513a0009a5957d8da5f66f88a
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-04-11 15:02:53 +0000

    Use ABI-specific pkg directories.
    Append a pkg suffix to REPOS_DIR in /etc, PKG_DBDIR and PKG_CACHEDIR.
    At the moment, the suffix is empty for a native aarch64c pkg or "64" for
    an aarch64 pkg.

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -100,13 +100,13 @@ static struct config_entry c[] = {
 	{
 		PKG_STRING,
 		"PKG_DBDIR",
-		"/var/db/pkg",
+		"/var/db/pkg" PKG_SUFFIX,
 		"Where the package databases are stored",
 	},
 	{
 		PKG_STRING,
 		"PKG_CACHEDIR",
-		"/var/cache/pkg",
+		"/var/cache/pkg" PKG_SUFFIX,
 		"Directory containing cache of downloaded packages",
 	},
 	{
@@ -148,7 +148,7 @@ static struct config_entry c[] = {
 	{
 		PKG_ARRAY,
 		"REPOS_DIR",
-		"/etc/pkg/,"PREFIX"/etc/pkg/repos/",
+		"/etc/pkg" PKG_SUFFIX "/," PREFIX "/etc/pkg/repos/",
 		"Location of the repository configuration files"
 	},
 	{
